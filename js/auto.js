b4b12507658a6a010a12a36b3c12be9f61264c55
carlos.jose.r2n <carlos.jose.r2n@gmail.com>
2019-10-17 18:18:37 -0300

    atualizacao dos arquivos e codificacao

---
+++ js/auto.js
@@ -0,0 +1,11 @@
+M.AutoInit();
+// document.addEventListener('DOMContentLoaded', function() {
+//     var elems = document.querySelectorAll('.scrollspy');
+//     var instances = M.ScrollSpy.init(elems, options);
+//   });
+
+  // Or with jQuery
+
+  $(document).ready(function(){
+    $('.scrollspy').scrollSpy();
+  });
